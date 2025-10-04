8c683b33ba31b2d3b71115be4cb4391d8631094b
Not-Tan <kritthongjib@gmail.com>
2025-10-04 17:30:32 +0700

    add financial_health_checker_tool.py and fhct_test

--- tests/fhct.py
+++ tests/fhct.py
@@ -7,8 +7,8 @@ executor = ToolExecutor()
 fhc = FinancialHealthCheckerTool()
 executor.register_tools(fhc)
 msg = ["please calculate my saving and investing ratio, I have 4000 rupees in savings, 5000 in investments and 3413243 in monthly incomes.",
-"please calculate my net worth, I have 10000 dollars in assets and 2000 in liabilities", "please calculate my liquidity ratio",
-"I have 5000 dollars in assets and 2000 in liabilities",
+"please calculate my net worth, I have 10000 dollars in assets and 2000 in liabilities",
+"please calculate my liquidity ratio, I have 5000 dollars in assets and 2000 in liabilities",
 "please calculate my emergency fund ratio, I have 2000 rupees in assets and 1000 in monthly nondiscreationary expenses",
 "please calculate my financial freedom ratio, I have 6000 in passive income and 10000 in expenses",
 "please calculate my financial freedom ratio, I have 6000 dollars in passive income and 10000 francs in expenses"]
